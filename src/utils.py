63b28d66d848a98afe243362c2374aa8d3e51ad4
shubranshugupta <57756826+shubranshugupta@users.noreply.github.com>
2022-05-01 02:50:28 +0530

    modify utils functions

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,19 @@
 import uuid
 import os
-import hashlib
 from cryptography.fernet import Fernet
+import yaml
+
+
+def read_yaml(file_path):
+    with open(file_path, 'r') as f:
+        return yaml.load(f, Loader=yaml.SafeLoader)
+
+def get_value(file_name, key):
+    file_path = os.path.join(os.getcwd(), file_name)
+    data = read_yaml(file_path)
+    for k in key.split("."):
+        data = data[k]
+    return data
 
 def create_id():
     return uuid.uuid1().hex
@@ -19,11 +31,11 @@ def decrypt(cypher:str):
 def password_check(passwd):     
     SpecialSym =['$', '@', '#']
 
-    if len(passwd) < 8:
-        return 'Length should be at least 6', False
+    if len(passwd) < 8 or len(passwd) > 20:
+        return 'Length should be between 8-20', False
           
     if not any(char.isdigit() for char in passwd):
-        return 'Password should have at least one numeral', False
+        return 'Password should have at least one number', False
           
     if not any(char.isupper() for char in passwd):
         return 'Password should have at least one uppercase letter', False
